d0c124377b5fdc5e8a432c2918bffff0e03704e6
Ahtisham01 <219668890+Ahtisham-1@users.noreply.github.com>
2026-07-27 23:11:46 +0530

    Created Taskcard component and rendered it on the display

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,32 @@ function App() {
   return (
     <>
       <h1>Kanban TaskBoard</h1>
-
-      <div className="columnDivs">
-        <h2>Todo</h2>
-      </div>
-      <div className="columnDivs">
-        <h2>In Progress</h2>
-        <TaskCard />
-      </div>
-      <div className="columnDivs">
-        <h2>Done</h2>
-      </div>
+      <main>
+        <div className="columnDivs">
+          <h2>Todo</h2>
+        </div>
+        <div className="columnDivs">
+          <h2>In Progress</h2>
+          <TaskCard
+            title="Clean Kitchen"
+            description="Clean the kitchen fully"
+            tag="hard"
+          />
+        </div>
+        <div className="columnDivs">
+          <h2>Done</h2>
+          <TaskCard
+            title="Do homework"
+            description="Do the homework of maths"
+            tag="tommorow homework check"
+          />
+          <TaskCard
+            title="Fix Fee Calculation"
+            description="Ensure balance updates after payment"
+            tag="FeeEase Bug"
+          />
+        </div>
+      </main>
     </>
   );
 }
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,9 +1,15 @@
-function TaskCard() {
+interface TaskCardProps {
+  title: string;
+  description: string;
+  tag: string;
+}
+
+function TaskCard({ title, description, tag }: TaskCardProps) {
   return (
     <div>
-     <h4>Task Title:</h4>
-     <p>Description:</p>
-     <span>Tag:</span>
+      <h4>{title}</h4>
+      <p>{description}</p>
+      <span>{tag}</span>
     </div>
   );
 }

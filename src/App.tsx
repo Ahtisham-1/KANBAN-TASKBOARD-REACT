a61c2007375c2acf1def7ecff3b563b70192e9ad
Ahtisham01 <219668890+Ahtisham-1@users.noreply.github.com>
2026-07-28 22:53:27 +0530

    added filter and map thing to the board now each board will show us weather our task is in pending or currently running or done

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,100 @@
 import TaskCard from "./components/TaskCard";
 
 function App() {
+  const tasks = [
+    {
+      id: 1,
+      title: "Clean Kitchen",
+      description: "Clean the kitchen fully",
+      tag: "easy",
+      column: "progress",
+    },
+    {
+      id: 2,
+      title: "wash face",
+      description: "wash you dirty face",
+      tag: "easy",
+      column: "progress",
+    },
+    {
+      id: 3,
+      title: "running",
+      description: "run 100 meters",
+      tag: "Medium",
+      column: "done",
+    },
+    {
+      id: 4,
+      title: "fighting",
+      description: "do some sparring",
+      tag: "hard",
+      column: "done",
+    },
+    {
+      id: 5,
+      title: "fighting",
+      description: "do some sparring",
+      tag: "hard",
+      column: "todo",
+    },
+    {
+      id: 6,
+      title: "fighting",
+      description: "do some sparring",
+      tag: "hard",
+      column: "todo",
+    },
+    {
+      id: 8,
+      title: "fighting",
+      description: "do some sparring",
+      tag: "hard",
+      column: "todo",
+    },
+  ];
+
   return (
     <>
       <h1>Kanban TaskBoard</h1>
       <main>
         <div className="columnDivs">
           <h2>Todo</h2>
+          {tasks
+            .filter((task) => task.column === "todo")
+            .map((item) => (
+              <TaskCard
+                key={item.id}
+                title={item.title}
+                description={item.description}
+                tag={item.tag}
+              />
+            ))}
         </div>
         <div className="columnDivs">
           <h2>In Progress</h2>
-          <TaskCard
-            title="Clean Kitchen"
-            description="Clean the kitchen fully"
-            tag="hard"
-          />
+          {tasks
+            .filter((task) => task.column === "progress")
+            .map((item) => (
+              <TaskCard
+                key={item.id}
+                title={item.title}
+                description={item.description}
+                tag={item.tag}
+              />
+            ))}
         </div>
         <div className="columnDivs">
           <h2>Done</h2>
-          <TaskCard
-            title="Do homework"
-            description="Do the homework of maths"
-            tag="tommorow homework check"
-          />
-          <TaskCard
-            title="Fix Fee Calculation"
-            description="Ensure balance updates after payment"
-            tag="FeeEase Bug"
-          />
+          {tasks
+            .filter((task) => task.column === "done")
+            .map((item) => (
+              <TaskCard
+                key={item.id}
+                title={item.title}
+                description={item.description}
+                tag={item.tag}
+              />
+            ))}
         </div>
       </main>
     </>
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,4 +1,5 @@
 interface TaskCardProps {
+  key: number;
   title: string;
   description: string;
   tag: string;
@@ -6,7 +7,7 @@ interface TaskCardProps {
 
 function TaskCard({ title, description, tag }: TaskCardProps) {
   return (
-    <div>
+    <div className="taskCardData">
       <h4>{title}</h4>
       <p>{description}</p>
       <span>{tag}</span>
